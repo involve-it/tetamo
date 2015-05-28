b2e830f670f0384450c9947e46eb9fc74a077f35
douson24 <infpartner@gmail.com>
2015-05-28 08:08:02 +0300

    Add css styles, changed code rendering view dialog

--- mobile-app/index.ios.js
+++ mobile-app/index.ios.js
@@ -5,111 +5,106 @@
 'use strict';
 
 var React = require('react-native');
+
 var {
-  AppRegistry,
-  Image,
-  ListView,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+    AppRegistry,
+    Image,
+    ListView,
+    StyleSheet,
+    Text,
+    View,
+    NavigatorIOS,
+    Component
+    } = React;
 
 var _ = require('lodash');
 var DDPClient = require("ddp-client");
+var styles = require('./main-style.js');
 
+/***********************************/
 var tetamo = React.createClass({
-  getInitialState: function() {
-    return {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => !_.isEqual(row1, row2),
-      }),
-      loaded: false,
-    };
-  },
+    getInitialState: function() {
+        return {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => !_.isEqual(row1, row2),
+            }),
+            loaded: false,
+        };
+    },
 
-  componentDidMount: function() {
-    var ddpClient = new DDPClient({url: 'ws://10.0.0.6:3000/websocket'});
+    componentDidMount: function() {
+        var ddpClient = new DDPClient({url: 'ws://192.168.1.2:3000/websocket'});
 
-    ddpClient.connect(() => ddpClient.subscribe('messages'));
+        ddpClient.connect(() => ddpClient.subscribe('messages'));
 
-    // observe the lists collection
-    var observer = ddpClient.observe("messages");
-    observer.added = (a) => {
-                               //console.log('Message : ' + JSON.stringify(ddpClient.collections.messages));
-                               //console.log('asdf');
-                               //console.log('Messages: ' + JSON.stringify(ddpClient.collections.messages));
-                               this.updateRows(_.cloneDeep(_.values(ddpClient.collections.messages)));
-                               //console.log('asdf1');
-                            }
-    //observer.changed = () => this.updateRows(_.cloneDeep(_.values(ddpClient.collections.lists)));
-    //observer.removed = () => this.updateRows(_.cloneDeep(_.values(ddpClient.collections.lists)));
-                               
-  },
+        // observe the lists collection
+        var observer = ddpClient.observe("messages");
+        observer.added = (a) => {
+            console.log('Message : ' + JSON.stringify(ddpClient.collections.messages));
+            //console.log('asdf');
+            //console.log('Messages: ' + JSON.stringify(ddpClient.collections.messages));
+            this.updateRows(_.cloneDeep(_.values(ddpClient.collections.messages)));
+            //console.log('asdf1');
+        }
+        //observer.changed = () => this.updateRows(_.cloneDeep(_.values(ddpClient.collections.lists)));
+        //observer.removed = () => this.updateRows(_.cloneDeep(_.values(ddpClient.collections.lists)));
 
-  updateRows: function(rows) {
-    //console.log('asdf2');
-    //console.log('rows: ' + JSON.stringify(rows));
-    this.setState({
-     dataSource: this.state.dataSource.cloneWithRows(rows),
-     loaded: true,
-   });
-  },
+    },
 
-  render: function() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
+    updateRows: function(rows) {
+        //console.log('asdf2');
+        //console.log('rows: ' + JSON.stringify(rows));
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(rows),
+            loaded: true,
+        });
+    },
+
+    render: function() {
+        if (!this.state.loaded) {
+            return this.renderLoadingView();
+        }
+
+        return (
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={this.renderList}
+                style={styles.listView}
+            />
+        );
+    },
+
+
+    renderLoadingView: function() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    Loading messages...
+                </Text>
+            </View>
+        );
+    },
+
+    renderList: function(message) {
+        if(message.keyMessage == 'friend-message') {
+            return (
+                <View style={styles.container}>
+                    <View  style={styles.friendContainer}>
+                        <Text style={styles.friendMessage}>{message.text}</Text>
+                    </View>
+                </View>
+            );
+        }
+        return (
+            <View style={styles.container}>
+                <View  style={styles.ownContainer}>
+                    <Text style={styles.ownMessage}>{message.text}</Text>
+                </View>
+            </View>
+        );
     }
-
-    return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderList}
-        style={styles.listView}
-      />
-    );
-  },
-
-  renderLoadingView: function() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          Loading messages...
-        </Text>
-      </View>
-    );
-  },
-
-  renderList: function(message) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.name}>{message.text}</Text>
-        <Text style={styles.incompleteCount}>{message.timestamp}</Text>
-      </View>
-    );
-  },
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    padding: 10,
-  },
-  name: {
-    flex: 5,
-    fontSize: 18,
-  },
-  incompleteCount: {
-    flex: 1,
-    textAlign: 'center',
-    fontSize: 20,
-    color: '#2196F3',
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: 'white',
-  },
-});
+/***********************************/
 
 AppRegistry.registerComponent('tetamo', () => tetamo);
---
+++ mobile-app/main-style.js
@@ -0,0 +1,61 @@
+module.exports = require('react-native').StyleSheet.create({
+    "listView": {
+        "backgroundColor": "#FFFFFF"
+    },
+    "container": {
+        "flexDirection": "column",
+        "justifyContent": "flex-start",
+        "alignItems": "flex-start",
+        "flexWrap": "nowrap",
+        "backgroundColor": "transparent",
+        "marginBottom": 5,
+        "paddingTop": 0,
+        "paddingBottom": 0,
+        "paddingRight": 15,
+        "paddingLeft": 15
+    },
+    "friendContainer": {
+        "alignSelf": "flex-end",
+        "borderWidth": 1,
+        "borderRadius": 2,
+        "borderColor": "transparent",
+        "backgroundColor": "rgba(238,241,244,1)",
+        "color": "rgba(63,73,82,1)",
+        "marginLeft": 60
+    },
+    "ownContainer": {
+        "alignSelf": "flex-start",
+        "borderWidth": 1,
+        "borderColor": "rgba(222,222,222,1)",
+        "borderRadius": 2,
+        "color": "rgba(63,73,82,1)",
+        "marginRight": 60
+    },
+    "friendMessage": {
+        "fontSize": 13,
+        "paddingTop": 10,
+        "paddingBottom": 10,
+        "paddingRight": 10,
+        "paddingLeft": 10
+    },
+    "ownMessage": {
+        "paddingTop": 10,
+        "paddingBottom": 10,
+        "paddingRight": 10,
+        "paddingLeft": 10,
+        "fontSize": 13
+    },
+    "welcome": {
+        "fontSize": 20,
+        "textAlign": "center",
+        "marginTop": 10,
+        "marginBottom": 10,
+        "marginRight": 10,
+        "marginLeft": 10
+    },
+    "instructions": {
+        "textAlign": "center",
+        "color": "#333333",
+        "marginBottom": 5
+    }
+});
